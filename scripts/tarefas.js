e1ea6710e709015640c3b963a9d499fd97295f50
Gabriel-Teodoro <gabrieltodoro1@hotmail.com>
2022-12-04 22:45:13 -0300

    conexão api e logica da pag cadastro, conexão api e logica pagina de tarefas, validações atualizadas e manipulação de DOM

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -1,47 +1,124 @@
-//necessario API para dar continuidade.
-//pegar nome usuario
-let nomeUsuario = document.getElementById('nomeUsuario')
+//agrupamento de variaveis DOM
+let form = document.getElementById('formTarefa');
+let itemTarefa = document.querySelector('#tarefasPendentes');
+let nomeUsuario = document.getElementById('nomeUsuario');
+let userJwt = sessionStorage.getItem("jwt");
+let listaPendente = document.getElementById('tarefasPendentes');
+let listaTerminada = document.getElementById('tarefasTerminadas');
+let inputTarefa = document.getElementById('novaTarefa');
+
+//onload atualiza lista de tasks e coloca função nos botões.
+document.addEventListener('DOMContentLoaded', function () {
+    atualizaTasks(userJwt);
+    stateBtn();
+})
+
+//teste (ignorar)
 nomeUsuario.innerText = 'teste'
 
-//botão envia pra api
-//POR ENQUANTO MANIPULAÇÃO DE DOM BASICA PARA TESTAR
-let form = document.getElementById('formTarefa')
-form.addEventListener('submit', function(event){
-    const date = new Date().toLocaleDateString();
-    let parent = document.getElementById('tarefasPendentes')
-    let inputTarefa = document.getElementById('novaTarefa')
-    let tarefa = `
-    <li class="tarefa">
-    <div class="not-done"></div>
-    <div class="descricao">
-      <p class="nome">${inputTarefa.value}</p>
-      <p class="timestamp">Criada em: ${date}</p>
-    </div>`
+//formulario de adicionar tarefa.
+form.addEventListener('submit', function (event) {
+    //normalizar variavel
+    descTask = normalizarTrim(inputTarefa.value);
 
+    //previnir evento default
     event.preventDefault();
-
     if (inputTarefa.value.length >= 5) {
-        parent.innerHTML += tarefa;
+        let post = {
+            description: descTask,
+            completed: false
+        }
+        let postJson = JSON.stringify(post)
+        postaTask(postJson);
     }
 })
 
-//mover entre listas
-let itemTarefa = document.querySelector('#tarefasPendentes')
-itemTarefa.addEventListener('click', function(event){
-    let btn = event.btn.closest('.not-done');
-    if (btn) {
-        console.log('foi')
+//fazer função async que atualiza a lista de tarefas e as organiza.
+async function atualizaTasks(jwt) {
+    //config
+    let requestConfig = {
+        headers: {
+            "authorization": `${jwt}`
+        }
     }
-})
+    console.log(requestConfig)
 
-// const listone = document.querySelector("#list");
-// const listwo = document.querySelector("#list2");
-// const li = listone.querySelectorAll("li");
+    //try/catch
+    try {
+        let lista = await fetch(`${baseUrl()}/tasks`, requestConfig)
+        console.log(lista)
+        if (lista.status == 200) {
+            let listaResponse = await lista.json();
+            attTask(listaResponse);
+        } else {
+            throw lista
+        }
+    } catch (error) {
+        console.log('caiu no catch')
+    }
+}
 
-// function MoveLi(){
-//   listwo.append(this);
-//   this.removeEventListener("click", MoveLi);
-// }
-// li.forEach( (el) => {
-//   el.addEventListener("click", MoveLi);
-// });
+//função que "atualiza task" adiciona uma task com o formato correto no html
+function attTask(array) {
+    console.log(array)
+    for (let i = 0; i < array.length; i++) {
+        let tarefa = `
+            <li class="nova-tarefa">
+                <div class="not-done"></div>
+                <div class="descricao">
+                    <p class="nome">${array[i].description}</p>
+                    <p class="timestamp">Criada em: ${array[i].createdAt}</p>
+                </div>
+            </li>`
+
+        if (array[i].completed === false) {
+            listaPendente.innerHTML += tarefa;
+            stateBtn()
+        } else if (array[i].completed === true) {
+            listaTerminada.innerHTML += tarefa;
+        }
+    }
+}
+
+//postar a task
+
+async function postaTask(response) {
+    let requestConfig = {
+        method: "POST",
+        body: response,
+        headers: {
+            "authorization": `${userJwt}`,
+            "content-Type": "application/json"
+        }
+    }
+
+    try {
+        let post = await fetch(`${baseUrl()}/tasks`, requestConfig);
+        if (post.status == 201) {
+            let postResponse = [await post.json()];
+            attTask(postResponse);
+            console.log('posta task FOI MERMAO')
+        }
+    } catch (error) {
+        console.log('posta task não foi')
+    }
+}
+
+//função dos botões (alteram o estado complete da task {PUT})
+function stateBtn() {
+    let btn = document.querySelectorAll('.not-done');
+    btn.forEach(btn => {
+        btn.addEventListener('click', function () {
+            //trazer função async {PUT} para atualizar post na api
+            listaTerminada.append(this.parentNode)
+        })
+    })
+}
+
+
+// adicionar botão para excluir e/ou retornar
+
+// adicionar alteração do nome com a função async getMe (colocar no DOMContentLoaded)
+
+// fazer o botão de finalizar sessão funcionar (limpar os dados no storage)
+
